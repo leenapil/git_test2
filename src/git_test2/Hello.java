043d14cb3d968a65a036cdf0c609a69db9457c2f
na hyun <lkg662130@naver.com>
2024-04-24 01:55:59 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,9 +2,17 @@ package git_test2;
 
 public class Hello {
 
-	String message = "안녕하세요";
+	String message;
 
-	public static void main(String[] args) {
-		System.out.println("message");
+	public Hello() {
+		this("안녕하세요");
+	}
+	
+	public Hello(String message) {
+		this.message = message;
+	}
+	
+	public void say() {
+		System.out.println(message);
 	}
 }
